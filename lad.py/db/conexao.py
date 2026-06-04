60c1de27195d328b93aa0858a9fc02b3e4976cbd
João Pedro <joaopedrocva21@gmail.com>
2026-06-04 14:26:14 -0300

    tentando desfazer cagada

--- lad.py/db/conexao.py
+++ lad.py/db/conexao.py
@@ -1,10 +1,13 @@
 import mysql.connector
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 def conectar():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="Jujyo9",
-        database="ladpy2"
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
     )
-
